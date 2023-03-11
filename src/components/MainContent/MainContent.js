76eb5efd5bd9b6b233e17bb0bcad479db1421d42
Marko2002Grujicic <m.grujicic2002@gmail.com>
2023-03-11 16:01:08 +0100

    first commit

---
+++ src/components/MainContent/MainContent.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Users } from "./User/Users";
+import "./MainContent.css"
+
+export const Main = () => {
+    return(
+        <div className="main-wrapper">
+            <Users/>
+        </div>
+    )
+}
---
+++ src/components/MainContent/User/Users.js
@@ -0,0 +1,29 @@
+import React, { useState} from "react";
+import { UserList } from "./RandomUsers";
+import './Users.css'
+
+export const Users = () => {
+    const [users] = useState(UserList);
+    const generateUser = users.sort(() => Math.random()).slice(0, 9);
+
+    return(
+        <div className="container">
+            {generateUser.map(user => (
+            
+                <div key={user.email} className="userProfile">
+                    
+                        <img src={user.picture.large} alt="user"/>
+                        <div className="userInfo">
+                            <p>Name: {user.name.first}</p>
+                            <p>Email: {user.email}</p>
+                            <p>Date of birth: {new Date(user.dob.date).toLocaleDateString()}</p>
+                        </div>
+                    
+                   
+                </div>
+            ))}
+        </div>
+    )
+
+
+}
